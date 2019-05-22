43752a5ca3f32e78501ed3d22ddc4faf458c818c
Zhang Yuteng <fellylanma@aliyun.com>
2019-05-22 21:32:54 +0800

    change the structure of the folders

---
+++ utest/main.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: Zhang Yuteng
+	> Mail:
+	> Created Time: 2019年05月22日 星期三 21时09分54秒
+ ************************************************************************/
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS() ;
+}
